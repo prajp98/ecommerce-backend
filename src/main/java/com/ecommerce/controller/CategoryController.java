21b1ce9f19684862eb37c4c4b380afd10c3c154f
Prajwala <praj.p98@gmail.com>
2026-05-07 18:47:25 +0530

    Added category controller

---
+++ src/main/java/com/ecommerce/controller/CategoryController.java
@@ -0,0 +1,59 @@
+package com.ecommerce.controller;
+
+import com.ecommerce.dto.request.CategoryRequest;
+import com.ecommerce.dto.response.CategoryResponse;
+import com.ecommerce.service.CategoryService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryResponse> createCategory(@Valid @RequestBody CategoryRequest request) {
+        CategoryResponse response = categoryService.createCategory(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryResponse> updateCategory(@PathVariable Long id,
+                                                           @Valid @RequestBody CategoryRequest request) {
+        CategoryResponse response = categoryService.updateCategory(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable Long id) {
+        CategoryResponse response = categoryService.getCategoryById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getAllCategories() {
+        List<CategoryResponse> response = categoryService.getAllCategories();
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/{id}/deactivate")
+    public ResponseEntity<CategoryResponse> deactivateCategory(@PathVariable Long id) {
+        CategoryResponse response = categoryService.deactivateCategory(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/{id}/activate")
+    public ResponseEntity<CategoryResponse> activateCategory(@PathVariable Long id) {
+        CategoryResponse response = categoryService.activateCategory(id);
+        return ResponseEntity.ok(response);
+    }
+}
