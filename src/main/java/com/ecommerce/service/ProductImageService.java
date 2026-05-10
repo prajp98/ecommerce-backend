dc3416e5f3720b46eacd5caaf23c0c2d9d28e181
Prajwala <praj.p98@gmail.com>
2026-05-10 23:19:26 +0530

    Created product image service

---
+++ src/main/java/com/ecommerce/service/ProductImageService.java
@@ -0,0 +1,15 @@
+package com.ecommerce.service;
+
+import com.ecommerce.dto.request.ProductImageRequest;
+import com.ecommerce.dto.response.ProductImageResponse;
+
+import java.util.List;
+
+public interface ProductImageService {
+
+    ProductImageResponse addImageToProduct(Long productId, ProductImageRequest request);
+
+    List<ProductImageResponse> getImagesByProductId(Long productId);
+
+    ProductImageResponse deleteImage(Long imageId);
+}
