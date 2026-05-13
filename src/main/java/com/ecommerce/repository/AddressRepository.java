d50f350e05ce7ad7c2d3199381989439e8187a04
prajwalaparameshwar <praj.p98@gmail.com>
2026-05-13 18:30:57 +0530

    Added address repo

---
+++ src/main/java/com/ecommerce/repository/AddressRepository.java
@@ -0,0 +1,16 @@
+package com.ecommerce.repository;
+
+import com.ecommerce.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    List<Address> findByUserId(Long userId);
+
+    Optional<Address> findByIdAndUserId(Long id, Long userId);
+
+    Optional<Address> findByUserIdAndDefaultAddressTrue(Long userId);
+}
