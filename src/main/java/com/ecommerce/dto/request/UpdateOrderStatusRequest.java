67c84d7ecc25a64586552d83e2d52557ccfa7821
prajwalaparameshwar <praj.p98@gmail.com>
2026-05-13 18:35:10 +0530

    Updated order status update

---
+++ src/main/java/com/ecommerce/dto/request/UpdateOrderStatusRequest.java
@@ -0,0 +1,25 @@
+package com.ecommerce.dto.request;
+
+import com.ecommerce.entity.OrderStatus;
+import jakarta.validation.constraints.NotNull;
+
+public class UpdateOrderStatusRequest {
+
+    @NotNull(message = "Order status is required")
+    private OrderStatus status;
+
+    public UpdateOrderStatusRequest() {
+    }
+
+    public UpdateOrderStatusRequest(OrderStatus status) {
+        this.status = status;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
