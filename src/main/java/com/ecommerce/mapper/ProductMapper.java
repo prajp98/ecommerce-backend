2b2e0c58d14dc3760fe2e2e0a18ece7a66be3472
prajwalaparameshwar <praj.p98@gmail.com>
2026-05-14 14:16:31 +0530

    Added product mapper

---
+++ src/main/java/com/ecommerce/mapper/ProductMapper.java
@@ -0,0 +1,18 @@
+package com.ecommerce.mapper;
+
+import com.ecommerce.dto.response.ProductResponse;
+import com.ecommerce.entity.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    @Mappings({
+            @Mapping(source = "category.id", target = "categoryId"),
+            @Mapping(source = "category.name", target = "categoryName"),
+            @Mapping(target = "message", ignore = true)
+    })
+    ProductResponse toResponse(Product product);
+}
