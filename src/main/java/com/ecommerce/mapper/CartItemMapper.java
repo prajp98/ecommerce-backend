1fec4de11e8b35a0e5c7d1d0e6b56697496d7c7b
prajwalaparameshwar <praj.p98@gmail.com>
2026-05-15 10:55:24 +0530

    Updated with mapper

---
+++ src/main/java/com/ecommerce/mapper/CartItemMapper.java
@@ -0,0 +1,18 @@
+package com.ecommerce.mapper;
+
+import com.ecommerce.dto.response.CartItemResponse;
+import com.ecommerce.entity.CartItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CartItemMapper {
+
+    @Mapping(source = "id", target = "cartItemId")
+    @Mapping(source = "product.id", target = "productId")
+    @Mapping(source = "product.name", target = "productName")
+    @Mapping(source = "product.price", target = "price")
+    @Mapping(target = "totalPrice", ignore = true)
+    @Mapping(target = "message", ignore = true)
+    CartItemResponse toResponse(CartItem cartItem);
+}
