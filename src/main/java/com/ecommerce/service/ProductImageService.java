e3c932782b68cae06806a2ea8c9b5f0e4d9cbb9f
prajwalaparameshwar <praj.p98@gmail.com>
2026-05-22 11:42:16 +0530

    Updated with image file upload

--- src/main/java/com/ecommerce/service/ProductImageService.java
+++ src/main/java/com/ecommerce/service/ProductImageService.java
@@ -1,13 +1,13 @@
 package com.ecommerce.service;
 
-import com.ecommerce.dto.request.ProductImageRequest;
 import com.ecommerce.dto.response.ProductImageResponse;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface ProductImageService {
 
-    ProductImageResponse addImageToProduct(Long productId, ProductImageRequest request);
+    ProductImageResponse addImageToProduct(Long productId, MultipartFile file, boolean primaryImage);
 
     List<ProductImageResponse> getImagesByProductId(Long productId);
 
